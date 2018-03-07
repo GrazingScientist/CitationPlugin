e707e2f2ddf3923259f32d38b8b39b33153e3f1d
Nate Wright <natew@notthisway.com>
2018-03-07 12:06:34 +0000

    Switch init method to register method

--- AddCtECitationStyle.inc.php
+++ AddCtECitationStyle.inc.php
@@ -4,10 +4,15 @@ import('lib.pkp.classes.plugins.GenericPlugin');
 
 class AddCtECitationStyle extends GenericPlugin {
 	/**
-	 * Hook into citation style
+	 * @copydoc Plugin::register()
 	 */
-	public function init() {
-		HookRegistry::register('CitationStyleLanguage::citationStyleDefaults', array($this, 'addCSLStyle'));
+	public function register($category, $path, $mainContextId = null) {
+		$success = parent::register($category, $path, $mainContextId);
+		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return $success;
+		if ($success && $this->getEnabled($mainContextId)) {
+			HookRegistry::register('CitationStyleLanguage::citationStyleDefaults', array($this, 'addCSLStyle'));
+		}
+		return $success;
 	}
 
 	/**

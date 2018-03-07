6ce30a32c54c4a879288b77c8d0fe8c71323559f
Nate Wright <natew@notthisway.com>
2018-03-07 12:01:20 +0000

    Fix indentation

--- AddCtECitationStyle.inc.php
+++ AddCtECitationStyle.inc.php
@@ -3,43 +3,43 @@
 import('lib.pkp.classes.plugins.GenericPlugin');
 
 class AddCtECitationStyle extends GenericPlugin {
-    /**
-     * Hook into citation style
-     */
-    public function init() {
-	HookRegistry::register('CitationStyleLanguage::citationStyleDefaults', array($this, 'addCSLStyle'));
-    }
+	/**
+	 * Hook into citation style
+	 */
+	public function init() {
+		HookRegistry::register('CitationStyleLanguage::citationStyleDefaults', array($this, 'addCSLStyle'));
+	}
 
-    /**
-     * Add a CSL style to the list of default styles
-     *
-     * @param string $hookname
-     * @param array $args [$defaults, CitationStyleLanguagePlugin]
-     */
-    public function addCSLStyle($hookName, $args) {
- 	$defaults =& $args[0];
+	/**
+	 * Add a CSL style to the list of default styles
+	 *
+	 * @param string $hookname
+	 * @param array $args [$defaults, CitationStyleLanguagePlugin]
+	 */
+	public function addCSLStyle($hookName, $args) {
+		$defaults =& $args[0];
 
-	$defaults[] = array(
-		'id' => 'contributions-to-entomology',
-		'title' => 'Contributions to Entomology',
-		'isEnabled' => true,
-		'useCsl' => '/srv/www/htdocs/journals/beitraege_entomology/plugins/generic/addCtECitationStyle/citationStyle.csl',
-	);
-    }
-    
-    /**
-     * @copydoc Plugin::getDisplayName()
-     */
-     function getDisplayName() {
-  	return ('Contributions to Entomology Citation Style');
-     }
-     
-     /**
-      * @copydoc Plugin::getDescription()
-      */
-      function getDescription() {
-	return ('Add the CtE citation style to the Citation Style Language.');
-      }
+		$defaults[] = array(
+			'id' => 'contributions-to-entomology',
+			'title' => 'Contributions to Entomology',
+			'isEnabled' => true,
+			'useCsl' => '/srv/www/htdocs/journals/beitraege_entomology/plugins/generic/addCtECitationStyle/citationStyle.csl',
+		);
+	}
+
+	/**
+	 * @copydoc Plugin::getDisplayName()
+	 */
+	function getDisplayName() {
+		return ('Contributions to Entomology Citation Style');
+	}
+
+	/**
+	 * @copydoc Plugin::getDescription()
+	 */
+	function getDescription() {
+		return ('Add the CtE citation style to the Citation Style Language.');
+	}
 }
 
 ?>

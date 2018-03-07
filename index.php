02fd2f4f2f3490dd7d7cd4510e0f0764a5881b59
Adrian Pachzelt <root@fifthseason.ub.uni-frankfurt.de>
2018-03-07 07:31:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file plugins/addCtECitationStyle/index.php
+ *
+ * @ingroup plugins_addCteCitationStyle
+ */
+require_once('AddCtECitationStyle.inc.php');
+return new AddCtECitationStyle();
+?>
